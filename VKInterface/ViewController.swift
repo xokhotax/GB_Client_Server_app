f3b7714e6591601fa2d128a3e17c21225e67f1c0
xokhotax <okhotai@gmail.com>
2021-11-14 09:42:07 +0100

    Initial Commit

---
+++ VKInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKInterface
+//
+//  Created by Ivan Okhota on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
