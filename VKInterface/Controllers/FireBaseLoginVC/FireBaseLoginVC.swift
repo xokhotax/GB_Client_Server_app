7a38be62f50f6b255eec940f397ec16133ac3021
xokhotax <okhotai@gmail.com>
2022-04-15 19:15:16 +0300

    HW 3 - Task 1 - FlyWeight pattern implementation

--- VKInterface/Controllers/FireBaseLoginVC/FireBaseLoginVC.swift
+++ VKInterface/Controllers/FireBaseLoginVC/FireBaseLoginVC.swift
@@ -10,111 +10,117 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class FireBaseLoginVC: UIViewController {
-  
-  @IBOutlet weak var loginTextField: UITextField!
-  @IBOutlet weak var passwordTextField: UITextField!
-  @IBOutlet weak var loginButton: UIButton!
-  @IBOutlet weak var registrationButton: UIButton!
-  
-  private var handle: AuthStateDidChangeListenerHandle!
-  
-  private let alertController = UIAlertController(title: "Зарегистрировать",
-                                                  message: "Провести регистрацию пользователя",
-                                                  preferredStyle: .actionSheet)
-  
-  private let alertMissedData = UIAlertController(title: "Ошибка",
-                                                  message: "Проверьте логин и пароль",
-                                                  preferredStyle: .alert)
-  
-  private let actionOK = UIAlertAction (title: "OK", style: .cancel)
-  private let actionNo = UIAlertAction (title: "Нет", style: .cancel)
-  
-  private let base = Firestore.firestore()
-  private let collection = "geekBrain"
-  
-  
-  private func messageWithServerError(error: Error){
-    let loginMissedData = UIAlertController(title: "Ошибка",
-                                            message: error.localizedDescription,
-                                            preferredStyle: .alert)
-    loginMissedData.addAction(self.actionOK)
-    self.present(loginMissedData, animated: true)
-  }
-  
-  private func registrationFireBase() -> UIAlertAction {
-    let actionYes = UIAlertAction(title: "Да", style: .default) {[weak self] _ in
-      guard let self = self,
-            let emailLogin = self.loginTextField.text,
-            let password = self.passwordTextField.text else { return }
-      
-      if emailLogin.count > 0, password.count > 0 {
-        Auth.auth().createUser(withEmail: emailLogin, password: password)
-      } else {
-        self.present(self.alertMissedData, animated: true)
-      }
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registrationButton: UIButton!
+    
+    private var handle: AuthStateDidChangeListenerHandle!
+    
+    private let alertController = UIAlertController(title: "Зарегистрировать",
+                                                    message: "Провести регистрацию пользователя",
+                                                    preferredStyle: .actionSheet)
+    
+    private let alertMissedData = UIAlertController(title: "Ошибка",
+                                                    message: "Проверьте логин и пароль",
+                                                    preferredStyle: .alert)
+    
+    private let actionOK = UIAlertAction (title: "OK", style: .cancel)
+    private let actionNo = UIAlertAction (title: "Нет", style: .cancel)
+    
+    private let base = Firestore.firestore()
+    private let collection = "geekBrain"
+    
+    
+    private func messageWithServerError(error: Error){
+        let loginMissedData = UIAlertController(title: "Ошибка",
+                                                message: error.localizedDescription,
+                                                preferredStyle: .alert)
+        loginMissedData.addAction(self.actionOK)
+        self.present(loginMissedData, animated: true)
     }
-    return actionYes
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    alertController.addAction(actionNo)
-    alertMissedData.addAction(actionOK)
-    alertController.addAction(registrationFireBase())
-  }
-  
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    Auth.auth().removeStateDidChangeListener(handle)
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    self .handle = Auth.auth().addStateDidChangeListener {[weak self] auth, user in
-      guard let self = self else { return }
-      if user != nil {
-        self.performSegue (withIdentifier: goToVKLogin , sender: nil )
-        self.loginTextField.text = nil
-        self.passwordTextField.text = nil
-      }
+    
+    private func registrationFireBase() -> UIAlertAction {
+        let actionYes = UIAlertAction(title: "Да", style: .default) {[weak self] _ in
+            guard let self = self,
+                  let emailLogin = self.loginTextField.text,
+                  let password = self.passwordTextField.text else { return }
+            
+            if emailLogin.count > 0, password.count > 0 {
+                Auth.auth().createUser(withEmail: emailLogin, password: password)
+            } else {
+                self.present(self.alertMissedData, animated: true)
+            }
+        }
+        return actionYes
     }
-  }
-  
-  @IBAction func loginButton(_ sender: UIButton) {
-    guard let emailLogin = loginTextField.text,
-          let password = passwordTextField.text,
-          emailLogin.count > 0,
-          password.count > 0 else {
+    
+    private func buttonColors(){
+        self.loginButton.tintColor = .custonmGrey
+        self.registrationButton.backgroundColor = .custonmGreyRed
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        alertController.addAction(actionNo)
+        alertMissedData.addAction(actionOK)
+        alertController.addAction(registrationFireBase())
+        buttonColors()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self .handle = Auth.auth().addStateDidChangeListener {[weak self] auth, user in
+            guard let self = self else { return }
+            if user != nil {
+                self.performSegue (withIdentifier: goToVKLogin , sender: nil )
+                self.loginTextField.text = nil
+                self.passwordTextField.text = nil
+            }
+        }
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        guard let emailLogin = loginTextField.text,
+              let password = passwordTextField.text,
+              emailLogin.count > 0,
+              password.count > 0 else {
             present(alertMissedData, animated: true)
             return
-          }
-    Auth.auth().signIn(withEmail: emailLogin, password: password) { [weak self] user, error in
-  
-      guard let user = user else { return }
-      
-      let model = UserFireBase(email: emailLogin, iD: user.user.uid, groupName: nil)
-      guard let data = try? JSONEncoder().encode(model),
-            let dict = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
-            let self = self
-      else { return }
-      
-      let usersFBSingleton = UsersFBSingleton.shared
-      usersFBSingleton.uid = user.user.uid
-      usersFBSingleton.email = emailLogin
-      
-      self.base.collection(_:self.collection).document(UsersFBSingleton.shared.uid).setData(dict, merge: true)
-      
-      if let error = error {
-        self.messageWithServerError(error: error)
-      }
+        }
+        Auth.auth().signIn(withEmail: emailLogin, password: password) { [weak self] user, error in
+            
+            guard let user = user else { return }
+            
+            let model = UserFireBase(email: emailLogin, iD: user.user.uid, groupName: nil)
+            guard let data = try? JSONEncoder().encode(model),
+                  let dict = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+                  let self = self
+            else { return }
+            
+            let usersFBSingleton = UsersFBSingleton.shared
+            usersFBSingleton.uid = user.user.uid
+            usersFBSingleton.email = emailLogin
+            
+            self.base.collection(_:self.collection).document(UsersFBSingleton.shared.uid).setData(dict, merge: true)
+            
+            if let error = error {
+                self.messageWithServerError(error: error)
+            }
+        }
     }
-  }
-  
-  @IBAction func registrationButton(_ sender: UIButton) {
-    present(alertController, animated: true)
-  }
-  
-  
-  
+    
+    @IBAction func registrationButton(_ sender: UIButton) {
+        present(alertController, animated: true)
+    }
+    
+    
+    
 }
 
---
+++ VKInterface/Extensions/UIColor.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor.swift
+//  VKInterface
+//
+//  Created by Ivan Okhota on 15.04.2022.
+//
+
+import UIKit
+
+extension UIColor{
+static let customOrange = UIColor(red: 255.0 / 255.0,
+                                  green: 100.0 / 255.0,
+                                  blue: 20.0 / 255.0,
+                                  alpha: 1.0)
+static let custonmWhite = UIColor(red: 250.0 / 255.0,
+                                  green: 250.0 / 255.0,
+                                  blue: 255.0 / 255.0,
+                                  alpha: 1.0)
+static let custonmGreyRed = UIColor(red: 220.0 / 255.0,
+                                    green: 110.0 / 255.0,
+                                    blue: 110.0 / 255.0,
+                                    alpha: 1.0)
+static let custonmGrey = UIColor(red: 150.0 / 255.0,
+                                 green: 150.0 / 255.0,
+                                 blue: 150.0 / 255.0,
+                                 alpha: 1.0)
+}
